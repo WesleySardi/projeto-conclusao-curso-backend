3e929e9ddf8d284e5638da93de954c8032bcf168
Wesley Sardi <wesley.sardi@gmail.com>
2024-11-19 21:41:34 -0300

    Altera a lógica do arquivo do token do Firebase.

--- src/main/java/org/example/biomedbacktdd/BiomedBackTddApplication.java
+++ src/main/java/org/example/biomedbacktdd/BiomedBackTddApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 @SpringBootApplication
 @EnableWebMvc
@@ -24,12 +26,23 @@ public class BiomedBackTddApplication {
     }
 
     private static void initializeFirebase() throws IOException {
+        String credentialsJson = System.getenv("FIREBASE_CREDENTIALS");
+
+        if (credentialsJson == null || credentialsJson.isEmpty()) {
+            throw new IllegalStateException("A variável de ambiente FIREBASE_CREDENTIALS não está definida.");
+        }
+
+        ByteArrayInputStream credentialsStream = new ByteArrayInputStream(credentialsJson.getBytes(StandardCharsets.UTF_8));
+
         FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.getApplicationDefault())
+                .setCredentials(GoogleCredentials.fromStream(credentialsStream))
                 .build();
 
         if (FirebaseApp.getApps().isEmpty()) {
             FirebaseApp.initializeApp(options);
+            System.out.println("Firebase inicializado com sucesso.");
+        } else {
+            System.out.println("Firebase já está inicializado.");
         }
     }
 }
